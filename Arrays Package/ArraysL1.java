2244e101bc4ba6e0aaf02bd212e1dfaa59d2e635
Krishna Singh <singhkrishna5099213@gmail.com>
2024-06-19 12:49:41 +0530

    1st days Commit

---
+++ Arrays Package/ArraysL1.java
@@ -0,0 +1,16 @@
+// package Arrays Package;
+
+public class ArraysL1{
+public static void main(String args[]){
+    // System.out.println("Hello World");
+    // System.out.println("Krishna Sigh");
+    int x  = 5;
+    int [] arr = new int[5];
+    arr [0] = 10;
+    arr [1] = 20;
+    arr [2] = 30;
+    arr [3] = 40;
+    arr [4] = 50;
+    System.out.println(arr[1]);
+}
+}
