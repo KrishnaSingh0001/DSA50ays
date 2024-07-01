ff6f94c027c217993d0ee1ee96562693edfa1d4c
Krishna Singh <singhkrishna5099213@gmail.com>
2024-07-01 06:05:48 +0530

    5th commit

---
+++ Arrays Package/BuiltinMethod.java
@@ -0,0 +1,12 @@
+// package ArraysPackage;
+public class BuiltinMethod {
+    public static void main(String[] args) {
+        int arr [] = {1 , 290, 35 , 48, 5};
+        for (int i = 0 ; i<arr.length;i++){
+            System.out.println(arr[i] + " ");
+        }
+        // Arrays.sort(arr[]);
+        // System.out.println(arr[i]);
+    }
+    
+}
